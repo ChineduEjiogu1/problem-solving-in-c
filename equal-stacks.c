4b7d05569b1afcdf24fd486f1c14f0de7f7702a3
ChineduEjiogu1 <chineduejiogu@gmail.com>
2024-01-09 10:55:34 -0500

    made some changes

--- equal-stacks.c
+++ equal-stacks.c
@@ -1,6 +1,11 @@
+#include <stdio.h>
+
 int equalStacks(int h1_count, int* h1, int h2_count, int* h2, int h3_count, int* h3) 
 {
-    
+   for(int i = 0; i < h1_count; i++)
+   {
+      h1[i];
+   }
 }
 
 int main()
